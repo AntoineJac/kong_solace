824e1c9066ccc48e17bae063a5d3be9904ad67b2
Antoine Jacquemin <ajacquem@gmail.com>
2025-02-13 13:50:19 +0100

    t

--- kong/plugins/debug-logging/schema.lua
+++ kong/plugins/debug-logging/schema.lua
@@ -1,27 +1,63 @@
 local typedefs = require "kong.db.schema.typedefs"
 
 local list_properties_forbidden = {
+  "SESSION_AUTHENTICATION_SCHEME",
   "SESSION_USERNAME",
   "SESSION_PASSWORD",
+  "SESSION_OAUTH2_ACCESS_TOKEN",
+  "SESSION_OIDC_ID_TOKEN",
   "SESSION_HOST",
-  "SESSION_PORT",
   "SESSION_VPN_NAME",
   "SESSION_CONNECT_TIMEOUT_MS",
-  "SESSION_CONNECT_BLOCKING"
+  "SESSION_WRITE_TIMEOUT_MS",
+  "SESSION_CONNECT_BLOCKING",
+  "SESSION_SEND_BLOCKING",
 }
 
 local PLUGIN_NAME = "debug-logging"
 
 
--- local function validate_properties(list_properties_forbidden)
---   for _, property in ipairs(properties) do
---     if not properties in list_properties then
---       return false, "Invalid property: " .. property
---     end
---   end
+local function is_empty(value)
+  return value == nil or value == ngx.null or value == ""
+end
 
---   return true
--- end
+local function validate_property_key(property)
+  if not property.property_key:match("^SESSION_") then
+    return false, "property key must start with 'SESSION_': " .. property.property_key
+  end
+  
+  for _, forbidden_key in ipairs(list_properties_forbidden) do
+    if property.property_key:match("^SOLCLIENT_SESSION") then
+      return false, "use the macro value not name : " .. property.property_key
+    end
+
+    if property.property_key == forbidden_key then
+      return false, "property forbidden: " .. property.property_key
+    end
+  end
+
+  return true
+end
+
+local function validate_config(config)
+  if config.session_authentication_scheme == "NONE" then
+    return true
+  end
+
+  if config.session_authentication_scheme == "AUTHENTICATION_SCHEME_BASIC" then
+    if is_empty(config.session_username) or is_empty(config.session_password) then
+      return false, "for basic: auth session_username and session_password should be provided"
+    end
+  end
+
+  if config.session_authentication_scheme == "AUTHENTICATION_SCHEME_OAUTH2" then
+    if is_empty(config.session_oauth2_access_token) and is_empty(config.session_oidc_id_token) then
+      return false, "for oauth2: one of session_oauth2_access_token or session_oidc_id_token should be provided"
+    end
+  end
+
+  return true
+end
 
 
 local schema = {
@@ -35,22 +71,150 @@ local schema = {
         type = "record",
         fields = {
           {
-            solace_sessions_propertires = {
+            solace_sessions_properties = {
               description = "the Solace session properties",
               type = "array",
-              elements = { type = "string" },
-              -- custom_validator = validate_sessions_properties,
+              len_max = 20,
+              elements = {
+                type = "record",
+                required = false,
+                fields = {
+                  {
+                    property_key = {
+                      type = "string",
+                      required = true,
+                    },
+                  },
+                  {
+                    property_value = {
+                      type = "string",
+                      required = true,
+                      referenceable = false, -- maybe true in future
+                    },
+                  },
+                },
+                custom_validator = validate_property_key,
+              },
+            },
+          },
+          { 
+            session_authentication_scheme = {
+              description = "Indicates the authentication used by the session to connect with Solace.",
+              required = true,
+              type = "string",
+              default = "AUTHENTICATION_SCHEME_BASIC", 
+              one_of = { "NONE", "AUTHENTICATION_SCHEME_BASIC", "AUTHENTICATION_SCHEME_OAUTH2" },
             },
           },
           {
-            log_scope = {
-              description = "Display the scope of the bearer token in the logs",
+            session_oauth2_access_token = {
+              type = "string",
+              required = false,
+              encrypted = true,
+              referenceable = true,
+            },
+          },
+          {
+            session_oidc_id_token = {
+              type = "string",
+              required = false,
+              encrypted = true,
+              referenceable = true,
+            },
+          },
+          {
+            session_username = {
+              type = "string",
+              required = false,
+            },
+          },
+          {
+            session_password = {
+              type = "string",
+              required = false,
+              encrypted = true,
+              referenceable = true,
+            },
+          },
+          {
+            session_vpn_name = {
+              type = "string",
+              required = false,
+            },
+          },
+          {
+            session_connect_timeout_ms = {
+              type = "integer",
               required = true,
-              type = "boolean",
-              default = false,
+              default = 3000,
+              between = { 100, 10000 },
+            },
+          },
+          {
+            session_write_timeout_ms = {
+              type = "integer",
+              required = true,
+              default = 3000,
+              between = { 100, 10000 },
+            },
+          },
+          {
+            increase_session_max_pool = {
+              type = "integer",
+              required = true,
+              between = { 0, 10 },
+              default = 2,
+            },
+          },
+          {
+            message_delivery_mode = {
+              type = "string",
+              required = true,
+              default = "DIRECT", 
+              one_of = { "DIRECT", "PERSISTENT", "NONPERSISTENT" },
+            },
+          },
+          {
+            message_destination_type = {
+              description = "You can use $(uri_captures['topic_name'] in this field",
+              type = "string",
+              required = true,
+              default = "TOPIC", 
+              one_of = { "TOPIC", "QUEUE" },
+            },
+          },
+          {
+            message_destination_name = {
+              type = "string",
+              required = true,
+              default = "tutorial/topic",
+            },
+          },
+          {
+            message_content_type = {
+              type = "string",
+              required = true,
+              default = "PAYLOAD", 
+              one_of = { "PAYLOAD", "CUSTOM" },
+            },
+          },
+          {
+            message_content_override = {
+              type = "string",
+              required = false,
+              default = "Hello World!",
+            },
+          },
+          {
+            ack_max_wait_time = {
+              type = "integer",
+              required = true,
+              default = 2000,
+              between = { 100, 5000 },
             },
           },
         },
+        custom_validator = validate_config,
       },
     },
   },
